ece748f71715df4c00ff9ea101de527ca3c43fc7
Vicproko <vicuproko@gmail.com>
2024-05-31 08:54:07 -0300

    ejercicio1 realizado

--- Assets/Scripts/AlquilerVehiculos.cs
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AlquilerVehiculos : MonoBehaviour
 {
-    public Vehiculo[] vehiculos;
+    public GameObject[] vehiculos;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +23,16 @@ public class AlquilerVehiculos : MonoBehaviour
 
     void ResetearVehiculos()
     {
+        DesactivarTodosLosElementos();
 
+        vehiculos[0].SetActive(true);
+    }
+
+    void DesactivarTodosLosElementos()
+    {
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
     }
 }
